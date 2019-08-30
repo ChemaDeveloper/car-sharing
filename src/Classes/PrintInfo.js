d6fdd11245ab4053ff0284e4ccda116286cce75a
U029534 <patricia.anton.ext@rsi.cajarural.com>
2019-08-30 10:09:19 +0200

    --

--- src/Classes/PrintInfo.js
+++ src/Classes/PrintInfo.js
@@ -1,19 +1,35 @@
 'use strict'
 let Users = require('../Classes/Users')
 let users = new Users(require('../utils/baseData.json'))
+const FUNCTIONS = require ('../utils/Functions')
 
 class PrintInfo {
   constructor() {
   }
+
+
   showInfo() {
     debugger;
-    let orderedUsers = users.getUserOrderByTimeDistance(users, {
-        position: JSON.parse(req.body.position), 
-        route: {
-          travelTime: req.body.fecha
-        }
+    console.log(users.userList.sort(FUNCTIONS.getSortMethod('+seats')))
+/*
+  getUserOrderByTimeDistance(users, keyUser) {
+    let dateTimeKey = FUNCTIONS.parseDateTime(keyUser.route.travelTime)
+    let currentDate = new Date();//dateTime momento de la peticion
+
+    let usersOrdered = []
+    this.userList.forEach(user => {
+      if (FUNCTIONS.parseDateTime(user.route.travelTime) >= currentDate) {
+            usersOrdered.push({
+              username: user.username,
+              distance: parseFloat(FUNCTIONS.calcDistance(keyUser.position, user.position)),
+              gapTime: FUNCTIONS.parseDateTime(user.route.travelTime) - dateTimeKey,
+              userRol: user.route.userRol
+            })
+          }
     })
-    console.log(orderedUsers);
+    return usersOrdered.sort(FUNCTIONS.orderUsersByDateTimeAndDistance)
+  }
+*/
     debugger;
       let travel = new Array;
       let nearest = new Array;
